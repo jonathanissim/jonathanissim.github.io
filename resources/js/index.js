7a7b1e6ff81347548ec4d2b614b9bc7978a2770a
jonathanissim <you@example.com>
2023-12-29 01:10:55 +0200

    Stylize grid

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
   generateCrosswordGrid();
 };
 
@@ -7,9 +7,14 @@ function generateCrosswordGrid() {
   const puzzleContainer = document.getElementById("puzzleContainer");
 
   for (let i = 0; i < 25; i++) {
-    const puzzleItem = document.createElement("div");
-    puzzleItem.classList.add("puzzle-item");
-    puzzleItem.textContent = i;
-    puzzleContainer.appendChild(puzzleItem);
+    const puzzleCell = document.createElement("input");
+    puzzleCell.classList.add("puzzle-item");
+    
+    // Replace character on input
+    puzzleCell.addEventListener("input", function () {
+      this.value = this.value.charAt(this.value.length - 1);
+    });
+
+    puzzleContainer.appendChild(puzzleCell);
   }
 }
