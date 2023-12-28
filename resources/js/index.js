0ddff79f22909387e6685768df2e8b2217d803df
jonathanissim <you@example.com>
2023-12-28 23:59:36 +0200

    Basic grid style

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,38 +1,15 @@
+window.onload = function() {
+  generateCrosswordGrid();
+};
 
-/**
-* Utility function to calculate the current theme setting.
-* Look for a local storage value.
-* Fall back to system setting.
-* Fall back to light mode.
-*/
-function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
-  if (localStorageTheme !== null) {
-    return localStorageTheme;
+
+function generateCrosswordGrid() {
+  const puzzleContainer = document.getElementById("puzzleContainer");
+
+  for (let i = 0; i < 25; i++) {
+    const puzzleItem = document.createElement("div");
+    puzzleItem.classList.add("puzzle-item");
+    puzzleItem.textContent = i;
+    puzzleContainer.appendChild(puzzleItem);
   }
-
-  if (systemSettingDark.matches) {
-    return "dark";
-  }
-
-  return "light";
 }
-
-/**
-* Utility function to update the button text and aria-label.
-*/
-function updateButton({ buttonEl, isDark }) {
-  const newCta = isDark ? "Change to light theme" : "Change to dark theme";
-  // use an aria-label if you are omitting text on the button
-  // and using a sun/moon icon, for example
-  buttonEl.setAttribute("aria-label", newCta);
-  buttonEl.innerText = newCta;
-}
-
-/**
-* Utility function to update the theme setting on the html tag
-*/
-function updateThemeOnHtmlEl({ theme }) {
-  document.querySelector("html").setAttribute("data-theme", theme);
-}
-
-
